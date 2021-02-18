1e3f6420c7afdc61037f00fa1b5bbfc2cb57fb82
HectorRad <a01636166@itesm.mx>
2021-02-17 23:45:08 -0600

    Laboratorio completo c-basics
    Signed-off-by: HectorRad <a01636166@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  3000     /* upper limit */
-#define   STEP   200      /* step size */
-
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char *argv[])
 {
     int fahr;
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    if (argc == 2){
+        fahr = atoi(argv[1]);
+        printf("The value in celsius is: %3f \n",(5.0/9.0)*(fahr-32));
+    }
+    else if(argc == 4){
+        int LOWER = atoi(argv[1]);
+        int UPPER = atoi(argv[2]);
+        int STEP = atoi(argv[3]);
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+    else{
+        printf("Wrong input");
+    }
+	
 
     return 0;
 }
