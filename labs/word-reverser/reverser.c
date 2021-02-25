589e72843e8673aee4dfeecd8e9c63b1851d929b
HectorRad <a01636166@itesm.mx>
2021-02-24 23:53:58 -0600

    Solve word-reverser
    Signed-off-by: HectorRad <a01636166@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+#define LIMITE 1000
+
+
+void reverse(char linea[]){
+  int i, j;
+  char aux;
+
+  for (i = 0; linea[i] != '\0'; ++i)
+    ;
+  --i;
+  
+  if (linea[i] == '\n')
+    --i;
+  
+  j = 0;
+  
+  while (j < i) {
+    aux = linea[j];
+    linea[j] = linea[i];
+    linea[i] = aux;
+    --i;
+    ++j;
+  }
+}
+
+int getLineas(char str[], int limite){
+  int i, c;
+
+  for (i = 0; i < limite-1 && (c = getchar()) != EOF && c != '\n'; ++i){
+    str[i] = c;
+  }
+
+  if (c == '\n'){
+    str[i] = c;
+    ++i;
+  }
+
+  str[i] = '\0';
+  return i;
+}
+
+int main(int argc, char **argv){
+  int tam;
+  char linea[LIMITE];
+
+  while ((tam = getLineas(linea, LIMITE)) > 0){
+    reverse(linea);
+    printf("%s\n", linea);
+  }
+
+  return 0;
 }
