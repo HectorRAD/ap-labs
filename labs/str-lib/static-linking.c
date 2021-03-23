8f6d4a6594fc912a07220426b2530e8e0a383deb
HectorRad <a01636166@itesm.mx>
2021-03-23 17:03:07 -0600

    Solved str-lib lab
    Signed-off-by: HectorRad <a01636166@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+
+    if (argc < 4 || argc > 4) {
+        printf("ERROR! Se necesitan tres argumentos, bajo el siguiente formato: \n");
+        printf("./main <funcion> <string1> <string2>\n");
+
+    }else{
+
+            if (strcmp(argv[1],"-add")==0) {
+
+                int lenght = mystrlen(argv[2]);
+                char *strNew = mystradd(argv[2], argv[3]);
+                int newlenght = mystrlen(strNew);
+                
+                printf("Initial Lenght\t   : %d\nNew String\t   : %s\nNew Lenght\t   : %d\n", lenght, strNew,newlenght);
+
+            }else if(strcmp(argv[1],"-find")==0) {
+
+                int pos = mystrfind(argv[2], argv[3]);
+
+                printf("['%s'] string was found at [%d] position\n",argv[3],pos);
+
+            }else{
+                printf("Funcion no valida, usar '-add' o '-find'\n");
+            }
+    }
+
+  return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,46 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int lenght = 0;
+    
+    for (int i = 0; str[i] != 0; i++) {
+        lenght++;
+    }
+
+    return lenght;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int origen = mystrlen(origin);
+    int addLen = mystrlen(addition);
+    char *nuevoStr = malloc(sizeof(char)*origen+ addLen);
+    
+    strcpy(nuevoStr, origin);
+    strcpy(nuevoStr + origen, addition);
+    return nuevoStr;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+
+  int aux = 0;
+
+  int origen = mystrlen(origin);
+  int subLen = mystrlen(substr);
+  for (int i = 0; i <= origen; i++) {
+    if (origin[i] == substr[0] && aux < 1) {
+      aux++;
+    }
+    else if (origin[i] == substr[aux]) {
+      aux++;
+    }
+    else {
+      aux = 0;
+    }
+    if (aux >= subLen) {
+      return aux;
+    }
+  }
+  return 0;
 }
