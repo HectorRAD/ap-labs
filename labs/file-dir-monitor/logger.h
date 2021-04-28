ff6e88745e8408c60f32d65c804f198a2d82580d
HectorRad <a01636166@itesm.mx>
2021-04-27 23:12:53 -0500

    Solved File directory monitor lab
    Signed-off-by: HectorRad <a01636166@itesm.mx>

--- labs/file-dir-monitor/logger.h
+++ labs/file-dir-monitor/logger.h
@@ -1,7 +1,15 @@
 // Logger
-
+#include <stdarg.h>
 int initLogger(char *logType);
 int infof(const char *format, ...);
 int warnf(const char *format, ...);
 int errorf(const char *format, ...);
 int panicf(const char *format, ...);
+void red();
+void boldred();
+void green();
+void yellow();
+void reset();
+int printStdOut(const char *format, va_list);
+int printSysLog(const char *format, va_list);
+int writeLog(const char *format, va_list arg);
